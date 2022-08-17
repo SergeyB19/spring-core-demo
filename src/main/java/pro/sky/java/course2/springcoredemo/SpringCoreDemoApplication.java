15e086923b3cb1db00b25fdec87b85457fb8f71b
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-08-17 12:28:11 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/springcoredemo/SpringCoreDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreDemoApplication.class, args);
+	}
+
+}
